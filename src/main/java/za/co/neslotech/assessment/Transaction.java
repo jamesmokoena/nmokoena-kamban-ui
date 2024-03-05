8eb954dc4256086e0c67e29f811fd21f906107bf
Ntethe James Mokoena <mokoenantethe@gmail.com>
2024-03-05 19:32:50 +0200

    created a Transaction class
    A transaction class will initiate an object with transactionId, sourceAcount, destinationAccount, amount and timestamp.

---
+++ src/main/java/za/co/neslotech/assessment/Transaction.java
@@ -0,0 +1,54 @@
+package za.co.neslotech.assessment;
+import java.time.LocalDateTime;
+
+
+public class Transaction {
+    private String transactionId;
+    private Account sourceAccount;
+    private Account destinationAccount;
+    private double amount;
+    private LocalDateTime timestamp;
+
+  
+
+    // Getters and setters
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Account getSourceAccount() {
+        return sourceAccount;
+    }
+
+    public void setSourceAccount(Account sourceAccount) {
+        this.sourceAccount = sourceAccount;
+    }
+
+    public Account getDestinationAccount() {
+        return destinationAccount;
+    }
+
+    public void setDestinationAccount(Account destinationAccount) {
+        this.destinationAccount = destinationAccount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
